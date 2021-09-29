ed5ab6e76dd615805fc93643f76b7d68b0f1037f
konifar <yahpeycoy0403@gmail.com>
2021-09-30 04:48:54 +0900

    chapter #14

--- util/random.go
+++ util/random.go
@@ -1,53 +1,53 @@
 package util
 
 import (
-	"math/rand"
-	"strings"
-	"time"
+    "math/rand"
+    "strings"
+    "time"
 )
 
 const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 func init() {
-	rand.Seed(time.Now().UnixNano())
+    rand.Seed(time.Now().UnixNano())
 }
 
 // RandomInt generates a random integer between min and max
 func RandomInt(min, max int64) int64 {
-	return min + rand.Int63n(max-min+1)
+    return min + rand.Int63n(max-min+1)
 }
 
 // RndomString generates a random string of length n
 func RandomString(n int) string {
-	var sb strings.Builder
-	k := len(alphabet)
+    var sb strings.Builder
+    k := len(alphabet)
 
-	for i := 0; i < n; i++ {
-		c := alphabet[rand.Intn(k)]
-		sb.WriteByte(c)
-	}
+    for i := 0; i < n; i++ {
+        c := alphabet[rand.Intn(k)]
+        sb.WriteByte(c)
+    }
 
-	return sb.String()
+    return sb.String()
 }
 
 // RandomOwner generates a random owner name
 func RandomOwner() string {
-	return RandomString(6)
+    return RandomString(6)
 }
 
 // RandomMoney generates a random amount of money
 func RandomMoney() int64 {
-	return RandomInt(0, 1000)
+    return RandomInt(0, 1000)
 }
 
 // RandomCurrency generates a random currency code
 func RandomCurrency() string {
-	currencies := []string{"EUR", "USD", "CAD"}
-	n := len(currencies)
-	return currencies[rand.Intn(n)]
+    currencies := []string{EUR, USD, CAD}
+    n := len(currencies)
+    return currencies[rand.Intn(n)]
 }
 
 // RandomMoney generates a random amount of money
 func RandomAccountId() int64 {
-	return RandomInt(1, 1000000)
+    return RandomInt(1, 1000000)
 }
